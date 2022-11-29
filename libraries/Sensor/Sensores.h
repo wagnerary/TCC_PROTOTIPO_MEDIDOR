6ca066c1c5262b901f52465b07a92a9155373b2d
Ary <wagnerary@hotmail.com>
2022-11-29 12:03:20 -0300

    Atualização da estrutura

--- libraries/Sensor/Sensores.h
+++ libraries/Sensor/Sensores.h
@@ -21,12 +21,12 @@
 
 #define IR_APPROXIMATION_VALUE  100000  // Valor minimo para o sensor detectar presenca.
 
-#define SATURATION_OFFSET       13      // Valor de ajuste para a saturacao
-#define SATURATION_MIN_VALUE    93      // Valor minimo para saturacao
+#define SATURATION_OFFSET       20      // Valor de ajuste para a saturacao
+#define SATURATION_MIN_VALUE    75      // Valor minimo para saturacao
 #define SATURATION_MAX_VALUE    99      // Valor maximo para saturacao
 #define SATURATION_ARR_SIZE     10      // Quantidade de elementos do array
 
-#define BEATPERMINUTE_OFFSET    0       // Valor de ajuste para batimentos
+#define BEATPERMINUTE_OFFSET    10      // Valor de ajuste para batimentos
 #define BEATPERMINUTE_MIN_VALUE 40      // Valor minimo para batimentos
 #define BEATPERMINUTE_MAX_VALUE 250     // Valor maximo para batimentos
 #define BEATPERMINUTE_ARR_SIZE  10      // Quantidade de elementos do array
@@ -39,19 +39,18 @@
 class Sensores
 {
   private:
-    static const int spO2LUT[43];
     bool  _beatDetection = false;
 
-    int   _saturationO2;
+    int   _saturationO2  = 0;
     int   _arrSaturationO2[SATURATION_ARR_SIZE];
     int   _countSaturation;
 
-    int   _beatPerMinute;
+    int   _beatPerMinute = 0;
     int   _arrBeatPerMinute[BEATPERMINUTE_ARR_SIZE];
     int   _countBeatPerMinute;
     float _timeBetweenBeats;
 
-    float _temperature;
+    float _temperature   = 0;
     float _arrTemperature[TEMPERATURE_ARR_SIZE];
     int   _countTemperature;
 
